4c9df85124575f30312082db9e085ab01c6f62c0
Hammad Khalid <109464146+hammadkhalid20@users.noreply.github.com>
2022-09-29 20:11:59 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-window.addEventListener("load", function(){
-    document.querySelector(".preloader").classList.add("opacity-0");
-    setTimeout(function(){
-        document.querySelector(".preloader").style.display="none";
-    },1000)
-})
-
-
 const nav=document.querySelector(".nav"),
 navList=nav.querySelectorAll("li"),
 totalNavList=navList.length,
